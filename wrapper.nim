8729b0e0b68e9ba6f6283a3df8cf74828a9c0ad9
Corey Lehman <coreylehman@me.com>
2022-04-09 17:50:32 -0400

    Cleaned up the wrapper a bit.

--- wrapper.nim
+++ wrapper.nim
@@ -4,52 +4,66 @@ export vst2
 type
   HostAudioBuffer* = ptr UncheckedArray[ptr UncheckedArray[cdouble]]
 
-  AudioPluginInitializer* = object
+  AudioPlugin* = ref object
     version*: int
     programCount*: int
     parameterCount*: int
     inputCount*: int
     outputCount*: int
-    processBlock*: proc(inputs, outputs: HostAudioBuffer, channelCount, blockSize: int)
+    processBlock*: proc(plugin: AudioPlugin, inputs, outputs: HostAudioBuffer, channelCount, blockSize: int)
+    effect: AEffect
 
-template audioPluginEntry*(initializer: AudioPluginInitializer): untyped =
-  proc vstDispatcher(effect: ptr AEffect, opcode, index: int32, value: int, `ptr`: pointer, opt: cfloat): int {.cdecl} =
-    case opCode:
+proc vstDispatcher(effect: ptr AEffect, opcode, index: int32, value: int, `ptr`: pointer, opt: cfloat): int {.cdecl} =
+  let plugin = cast[AudioPlugin](effect.`object`)
 
-    of effClose.int32:
-      dealloc(effect)
+  case opCode:
 
-    of effGetVendorVersion.int32:
-      return effect.version
+  of effClose.int32:
+    GC_unref(plugin)
 
-    else:
-      discard
+  of effGetVendorVersion.int32:
+    return effect.version
 
-  proc vstGetParameter(effect: ptr AEffect, index: int32): cfloat {.cdecl.} =
+  else:
     discard
 
-  proc vstSetParameter(effect: ptr AEffect, index: int32, parameter: cfloat) {.cdecl.} =
-    discard
+proc vstGetParameter(effect: ptr AEffect, index: int32): cfloat {.cdecl.} =
+  discard
 
-  proc vstProcessDoubleReplacing(effect: ptr AEffect, inputs, outputs: ptr ptr cdouble, sampleFrames: int32) {.cdecl.} =
-    var inputs = cast[HostAudioBuffer](inputs)
-    var outputs = cast[HostAudioBuffer](outputs)
-    initializer.processBlock(inputs, outputs, effect.numInputs, sampleFrames)
+proc vstSetParameter(effect: ptr AEffect, index: int32, parameter: cfloat) {.cdecl.} =
+  discard
 
+proc vstProcessDoubleReplacing(effect: ptr AEffect, inputs, outputs: ptr ptr cdouble, sampleFrames: int32) {.cdecl.} =
+  let plugin = cast[AudioPlugin](effect.`object`)
+  var inputs = cast[HostAudioBuffer](inputs)
+  var outputs = cast[HostAudioBuffer](outputs)
+  plugin.processBlock(plugin, inputs, outputs, effect.numInputs, sampleFrames)
+
+proc newAudioPlugin*(version: int,
+                     programCount: int,
+                     parameterCount: int,
+                     inputCount: int,
+                     outputCount: int): AudioPlugin =
+  result = AudioPlugin(
+    effect: AEffect(
+      magic: CCONST('V', 's', 't', 'P'),
+      flags: effFlagsCanDoubleReplacing,
+      uniqueID: CCONST('A', 'S', 'D', 'F'),
+      version: version.int32,
+      numParams: parameterCount.int32,
+      numPrograms: programCount.int32,
+      numInputs: inputCount.int32,
+      numOutputs: outputCount.int32,
+      dispatcher:  vstDispatcher,
+      getParameter: vstGetParameter,
+      setParameter: vstSetParameter,
+      processDoubleReplacing: vstProcessDoubleReplacing,
+    ),
+  )
+  result.effect.`object` = cast[pointer](result)
+
+template exportAudioPlugin*(p: AudioPlugin): untyped =
   proc VSTPluginMain(vstHostCallback: AEffect): ptr AEffect {.exportc, dynlib.} =
-    var effect = create(AEffect)
-
-    effect.magic = CCONST('V', 's', 't', 'P')
-    effect.flags = effFlagsCanDoubleReplacing
-    effect.uniqueID = CCONST('A', 'S', 'D', 'F')
-    effect.version = initializer.version.int32
-    effect.numParams = initializer.parameterCount.int32
-    effect.numPrograms = initializer.programCount.int32
-    effect.numInputs = initializer.inputCount.int32
-    effect.numOutputs = initializer.outputCount.int32
-    effect.dispatcher =  vstDispatcher
-    effect.getParameter = vstGetParameter
-    effect.setParameter = vstSetParameter
-    effect.processDoubleReplacing = vstProcessDoubleReplacing
-
-    return effect
+    let pluginRef = p
+    GC_ref(pluginRef)
+    return pluginRef.effect.addr
